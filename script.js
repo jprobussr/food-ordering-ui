e05ffd1204607f6c1777fa76e8b78b7739ec029a
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2026-04-04 15:13:02 -0500

    feat: add initial cart array and button click logic

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ const favoriteButtons = document.querySelectorAll('.favorite-btn');
 const themeToggle = document.querySelector('.theme-toggle');
 const savedTheme = localStorage.getItem('theme');
 
+const addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
+const cart = [];
+
+addToCartButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const itemName = button.dataset.name;
+    const itemPrice = Number(button.dataset.price);
+
+    const item = {
+      name: itemName,
+      price: itemPrice,
+    };
+
+    cart.push(item);
+    console.log(cart);
+  });
+});
+
 if (savedTheme === 'light') {
   document.body.classList.add('light');
   themeToggle.textContent = '☀️';
